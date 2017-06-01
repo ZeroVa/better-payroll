70d34b2a3dcb08d4cd350e0a41c06c9426c5ee60
Mojache1234 <arielvongoco@gmail.com>
2017-06-01 12:36:39 -0700

    edited users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  // check if saved
   res.send('respond with a resource');
+});
+
+router.get('/users:userid/sessions', function(req, res, next) {
+  res.send('listprograms');
 });
 
 module.exports = router;
---
+++ src/components/programs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class DisplayPrograms extends React.Component {
+    render() {
+        var Programs = ["Bible Way", ] // this should actually be coming from database
+        
+    }
+}
